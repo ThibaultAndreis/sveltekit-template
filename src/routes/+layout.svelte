d20ae4ef6ee029b1e156a9baa4676f6410f7dbef
Thibault <thibault@sports-aventure.fr>
2023-11-06 02:27:44 +0100

    update svelte:head to take .env value

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
+	import { PUBLIC_DEFAULT_URL } from "$env/static/public";
 	import Header from '$lib/component/Header.svelte';
 	import { themeStore } from '$lib/stores/settings';
 	import '../app.pcss';
@@ -25,9 +26,10 @@
 <svelte:head>
 	<meta name="description" content={$t('common.Description')} />
 	<title>{$t('common.Title')}</title>
+	<link rel="canonical" href="https://{PUBLIC_DEFAULT_URL}" />
 
 	<!-- Facebook Meta Tags -->
-	<meta property="og:url" content="https://www.insert-name-here.com/" />
+	<meta property="og:url" content="https://{PUBLIC_DEFAULT_URL}" />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content="Thibault - {$t('common.Title')}" />
 	<meta property="og:description" content={$t('common.Description')} />
@@ -35,8 +37,8 @@
 
 	<!-- Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta property="twitter:domain" content="insert-name-here.com" />
-	<meta property="twitter:url" content="https://www.insert-name-here.com/" />
+	<meta property="twitter:domain" content={PUBLIC_DEFAULT_URL} />
+	<meta property="twitter:url" content="https://{PUBLIC_DEFAULT_URL}" />
 	<meta name="twitter:title" content="Thibault - {$t('common.Title')}" />
 	<meta name="twitter:description" content={$t('common.Description')} />
 	<meta name="twitter:image" content="" />
